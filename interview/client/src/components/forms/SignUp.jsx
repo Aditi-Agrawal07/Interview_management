0b6d165b7698c96d55a4ba3185e6dd0d1e438c75
mangalsingh123t <121483110+mangalsingh123t@users.noreply.github.com>
2024-08-17 11:24:00 +0530

    Comlete the SignUp Form and Implement Validation in it

--- interview/client/src/components/forms/SignUp.jsx
+++ interview/client/src/components/forms/SignUp.jsx
@@ -1,44 +1,103 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export const SignUp = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            class: '',
+            level: '',
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .max(15, 'Name Must be 15 characters or less').min(3,'Name Must be min 3 characters')
+                .required('Required'),
+            email: Yup.string()
+                .email('Invalid email address')
+                .max(20, 'Must be 20 characters or less')
+                .required('Required'),
+            class: Yup.string().required('Required'),
+            level: Yup.string().required('Required'),
+        }),
+        onSubmit: (values,{resetForm}) => {
+            console.log(values);
+            resetForm();
+        },
+    });
+
     return (
         <>
-            <div className="container-fluid w-full h-screen  flex justify-center items-center">
-                <div className="container mx-auto bg-white h-96 w-1/2 p-4 rounded-md shadow-xl" >
-                    <div className="text-center font-bold text-2xl pt-5">Registration Form</div>
-                    <form>
-                        <div className="flex justify-center mt-5">
-                            <input className="outline-none w-1/2 mb-4 p-2 border" type="text" placeholder="name" />
+            <div className="container-fluid w-full h-screen flex justify-center items-center bg-gray-100">
+                <div className="container mx-auto bg-white h-4/5 w-1/3 p-4 rounded-md shadow-xl">
+                    <div className="text-center font-bold text-2xl pt-5">Interview Form</div>
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className="flex flex-col mt-5 mx-auto w-2/3 mb-2">
+                            <input 
+                                className="outline-none  p-2 border" 
+                                type="text" 
+                                placeholder="Name" 
+                                {...formik.getFieldProps('name')}
+                            />
+                            {formik.touched.name && formik.errors.name ? (
+                                <div className=" text-red-500 text-sm mt-1">{formik.errors.name}</div>
+                            ) : null}
                         </div>
-                        <div className="flex justify-center">
-                            <input className="outline-none  w-1/2 p-2 mb-4 border" type="email" placeholder="email" />
+                        <div className="flex flex-col mt-5 mx-auto w-2/3 mb-2">
+                            <input 
+                                className="outline-none  p-2  border" 
+                                type="email" 
+                                placeholder="Email" 
+                                {...formik.getFieldProps('email')}
+                            />
+                            {formik.touched.email && formik.errors.email ? (
+                                <div className="text-red-500 text-sm mt-1">{formik.errors.email}</div>
+                            ) : null}
                         </div>
-                        <div className="flex justify-center">
-                            <select className="mb-4 outline-none w-1/2 p-2 border text-gray-500" id="class" name="class">
-                                <option value="" disabled selected>Class</option>
+                        <div className="flex flex-col mt-5 mx-auto w-2/3 mb-2">
+                            <select 
+                                className=" outline-none  p-2 border text-gray-500" 
+                                id="class" 
+                                name="class" 
+                                {...formik.getFieldProps('class')}
+                            >
+                                <option value="" disabled>Class</option>
                                 <option value="1st year">1st Year</option>
                                 <option value="2nd year">2nd Year</option>
                                 <option value="3rd year">3rd Year</option>
                                 <option value="1st year diploma">1st Year Diploma</option>
                                 <option value="2nd year diploma">2nd Year Diploma</option>
                             </select>
+                            {formik.touched.class && formik.errors.class ? (
+                                <div className="text-red-500 text-sm mt-1">{formik.errors.class}</div>
+                            ) : null}
                         </div>
-                        <div className="flex justify-center">
-                            <select className="mb-4 outline-none w-1/2 p-2 border text-gray-500" id="leve;" name="level">
-                                <option value="" disabled selected>Level</option>
+                        <div className="flex flex-col mt-5 mx-auto w-2/3 mb-2">
+                            <select 
+                                className=" outline-none  p-2 border text-gray-500" 
+                                id="level" 
+                                name="level" 
+                                {...formik.getFieldProps('level')}
+                            >
+                                <option value="" disabled>Level</option>
                                 <option value="1A">1A</option>
                                 <option value="1B">1B</option>
                                 <option value="1C">1C</option>
                                 <option value="2A">2A</option>
                                 <option value="2B">2B</option>
-                                <option value="2B">2C</option>
+                                <option value="2C">2C</option>
                             </select>
+                            {formik.touched.level && formik.errors.level ? (
+                                <div className="text-red-500 text-sm mt-1">{formik.errors.level}</div>
+                            ) : null}
                         </div>
-                        <div className="text-center">
-                            <button className="p-1 bg-blue-500 rounded-md px-3" type="submit">Submit</button>
+                        <div className="text-center mx-auto w-2/3">
+                            <button className="p-1 block w-full bg-custom-bg rounded-md mt-4 text-white" type="submit">Submit</button>
                         </div>
                     </form>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
